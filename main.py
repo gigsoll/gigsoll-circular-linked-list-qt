c8fb0102db628bacbf4609ea9f2d824218df4abf
gigsoll <gigsollt@gmail.com>
2024-04-12 18:06:41 +0300

    Added bindings to the buttons

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ class Ui(QWidget):
         self.show()
         self.cbList.addItems({"Учні", "Білети"})
 
+        self.pbAdd.clicked.connect(self.pbAdd_click)
+        self.pbDelete.clicked.connect(self.pbDelete_click)
+        self.pbFind.clicked.connect(self.pbFind_click)
+        self.pbGenerate.clicked.connect(self.pbGenerate_click)
+        self.pbGiveTickets.clicked.connect(self.pbGiveTickets_click)
+
+        self.students = CircularLinkedList()
+        self.tickets = CircularLinkedList()
+
+    def pbAdd_click(self):
+        print("Add")
+
+    def pbDelete_click(self):
+        print("Delete")
+
+    def pbFind_click(self):
+        print("Find")
+
+    def pbGenerate_click(self):
+        print("Generate")
+
+    def pbGiveTickets_click(self):
+        print("Give Tickets")
+
 app = QApplication(sys.argv)
 window = Ui()
 app.exec()
