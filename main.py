5e279e9f60b48faa02682df26a08383d7bc57537
gigsoll <gigsollt@gmail.com>
2024-04-12 18:36:11 +0300

    Added methods to generate random names and tickets

--- main.py
+++ main.py
@@ -36,6 +36,20 @@ class Ui(QWidget):
     def pbGiveTickets_click(self):
         print("Give Tickets")
 
+    def GenerateNames(self, length):
+        names = ["Андрій", "Ярослав", "Олександр", "Михайло", "Богдан", "Олена", "Катерина", "Марія", "Анна", "Наталія"]
+        surnames = ["Шевченко", "Петренко", "Кравчук", "Іваненко", "Коваленко", "Степаненко", "Сидоренко", "Поліщук", "Бордар", "Ткаченко"]
+        students = []
+        for i in range(length):
+            students.append(f"{random.choice(names)} {random.choice(surnames)}")
+        return students
+    
+    def GenerateTickets(self, length):
+        tickets = []
+        for i in range(length):
+            tickets.append(random.randint(1000, 10000))
+        return tickets
+    
 app = QApplication(sys.argv)
 window = Ui()
 app.exec()
